775979892964ec4dac7c1c6c545aba9f25dcd1f5
Mirko Colli <collidesign@gmail.com>
2020-03-18 15:42:53 +0100

    Aggiunto ASP.NET Core MVC

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,10 +28,10 @@ namespace MyCourse
             }
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder =>
             {
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Ciao Hello {nome.ToUpper()}!");
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
